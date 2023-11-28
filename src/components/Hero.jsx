2715a64ea8c1a9b27374d110cbb0489c482985f8
UrvikP <urvik95@gmail.com>
2023-11-28 12:55:51 -0500

    Update Hero.jsx
    Added a heading and description of the website.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { logo } from '../assets'
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
 
         <button type='button' onClick={() => window.open('https://github.com/UrvikP/AI_Summarizer')}
@@ -11,6 +11,17 @@ const Hero = () => {
           Github
         </button>
       </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Don't have the time to read the entire article? 
+        Well you came to the right place! SumzUp is an 
+        open-source article summarizer that transforms 
+        lengthy articles into clear and concise summaries.
+      </h2>
     </header>
   )
 }
